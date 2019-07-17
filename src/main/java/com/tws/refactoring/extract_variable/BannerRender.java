2cb5d9a9029d4a02a0aa024c688d0d607810d516
Chaowei Zeng <sli616@163.com>
2019-07-17 14:04:39 +0800

    refactor BannerRender and pass the tests.

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -3,11 +3,18 @@ package com.tws.refactoring.extract_variable;
 import com.sun.istack.internal.NotNull;
 
 public class BannerRender {
+    private static final String MAC = "MAC";
+    private static final String IE = "IE";
+    private static final String IE_ON_MAC = "IE on Mac?";
+    private static final String BANNER = "banner";
+
     String renderBanner(@NotNull String platform, @NotNull String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
-        return "banner";
+        return containsIgnoreCase(platform, MAC) && containsIgnoreCase(browser, IE)
+                ? IE_ON_MAC
+                : BANNER;
+    }
+
+    private boolean containsIgnoreCase(String string, String target) {
+        return string.toUpperCase().contains(target.toUpperCase());
     }
 }
